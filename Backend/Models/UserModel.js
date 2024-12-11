2abe70dd1393b1c9c5ed5c6268d30da8be2a187f
prem-thatikonda29 <2023.tprem@isu.ac.in>
2024-12-11 14:23:52 +0530

    added registration api

--- Backend/Models/UserModel.js
+++ Backend/Models/UserModel.js
@@ -1,12 +1,15 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  username: { type: String, required: true, unique: true },
-  email: { type: String, required: true },
-  password: { type: String, required: true },
-  isAdmin: { type: Boolean, required: true, default: false },
-});
+const userSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    isAdmin: { type: Boolean, required: true, default: false },
+  },
+  { timestamps: true }
+);
 
 const userModel = mongoose.model("users", userSchema);
 
--- Backend/Routes/AuthRouter.js
+++ Backend/Routes/AuthRouter.js
@@ -1,8 +1,12 @@
 import express from "express";
-import { getUsers } from "../Controllers/AuthController.js";
+import { getUsers, registerUser } from "../Controllers/AuthController.js";
 
 const authRouter = express.Router();
 
 authRouter.get("/", getUsers);
 
+authRouter.post("/register", registerUser);
+
 export default authRouter;
+
+// 
