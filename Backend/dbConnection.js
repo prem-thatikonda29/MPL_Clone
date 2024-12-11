339c4a75d3941fd40cead5632005b93c9bb14713
prem-thatikonda29 <2023.tprem@isu.ac.in>
2024-12-11 13:06:26 +0530

    Added usermodel

---
+++ Backend/dbConnection.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+function dbConnector() {
+  mongoose
+    .connect("mongodb://localhost:27017/MPL", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => {
+      console.log("Connected to MongoDB");
+    })
+    .catch((err) => {
+      console.log("Failed to connect to MongoDB", err);
+    });
+}
+
+export default dbConnector;
--- Backend/index.js
+++ Backend/index.js
@@ -1,5 +1,8 @@
 import express from "express";
+import dbConnector from "./dbConnection.js";
 const app = express();
+
+dbConnector();
 
 app.listen(8000, () => {
   console.log("Server is running on port 8000");
