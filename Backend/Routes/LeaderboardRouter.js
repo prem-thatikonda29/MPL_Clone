c3e642db09d1b08be7ed5cfe4af3cb1875e81a60
prem-thatikonda29 <2023.tprem@isu.ac.in>
2024-12-13 14:22:05 +0530

    Added game routes, leaderboard routes

--- Backend/Routes/LeaderboardRouter.js
+++ Backend/Routes/LeaderboardRouter.js
@@ -2,18 +2,14 @@ import express from "express";
 import {
   addToLeaderboard,
   getLeaderboard,
-} from "../Controllers/LeaderboardController.js"; // Adjust the path to your controller
-import leaderboardModel from "../Models/LeaderboardModel.js";
+} from "../Controllers/LeaderboardController.js";
 
 const leaderboardRouter = express.Router();
 
 // Route to get the leaderboard for a specific game
 leaderboardRouter.get("/:gameId", getLeaderboard);
 
+// Route to add a new leaderboard entry
 leaderboardRouter.post("/add", addToLeaderboard);
-
-// You can add more routes here, such as posting new leaderboard entries (high scores).
-// For example:
-// leaderboardRouter.post("/add", addToLeaderboard);
 
 export default leaderboardRouter;
