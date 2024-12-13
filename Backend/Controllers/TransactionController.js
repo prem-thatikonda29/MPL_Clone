7248bb024e4127df59735a4fbdd3cf15a5c04e09
prem-thatikonda29 <2023.tprem@isu.ac.in>
2024-12-13 12:13:40 +0530

    optimised the transaction function to create a transaction

--- Backend/Controllers/TransactionController.js
+++ Backend/Controllers/TransactionController.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 
 import TransactionModel from "../Models/TransactionModel.js";
+import userModel from "../Models/UserModel.js";
 
 export async function getTransactions(req, res) {
   try {
@@ -18,11 +19,32 @@ export async function getTransactions(req, res) {
 export async function createTransaction(req, res) {
   try {
     const body = req.body;
-    const newTransaction = await TransactionModel.create(body);
+    const user = await userModel.findById(body.uid);
 
+    if (!user) {
+      return res.status(404).send("User not found");
+    }
+
+    const newTransaction = await TransactionModel.create(body);
     if (!newTransaction) {
       return res.status(400).send("Invalid data");
     }
+
+    if (newTransaction.transaction.typeOfTransaction === "debit") {
+      if (user.balance < newTransaction.transaction.amount) {
+        return res.status(400).send("Insufficient balance");
+      }
+      const updatedUser = await user.updateOne({
+        $set: { balance: user.balance - newTransaction.transaction.amount },
+      });
+    } else if (newTransaction.transaction.typeOfTransaction === "credit") {
+      const updatedUser = await user.updateOne({
+        $set: { balance: user.balance + newTransaction.transaction.amount },
+      });
+    } else {
+      return res.status(400).send("Invalid data");
+    }
+
     res.status(201).send(newTransaction);
   } catch (error) {
     res.status(500).send({ message: error.message });
